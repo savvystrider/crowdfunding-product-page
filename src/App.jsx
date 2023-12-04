41d16ee39854cd28789a8dea75c103128a3ef51d
Manish <mjpatel84@gmail.com>
2023-12-04 15:10:52 -0500

    adds ProjectMain component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import ProjectMain from "./components/ProjectMain";
 
 export default function App() {
   return (
     <>
       <Header />
+      <ProjectMain />
     </>
   );
 }
---
+++ src/components/ProjectMain.jsx
@@ -0,0 +1,23 @@
+export default function ProjectMain() {
+  return (
+    <section className="project-main">
+      <div className="img-container">
+        <img
+          src="assets/logo-mastercraft.svg"
+          alt="Mastercraft logo"
+          className="mastercraft-logo"
+        />
+      </div>
+      <div className="project-main_main-text">
+        <h1>Mastercraft Bamboo Monitor Riser</h1>
+        <p className="subtitle">
+          A beautiful & handcrafted monitor stand to reduce neck and eye strain.
+        </p>
+      </div>
+      <div className="btn-container">
+        <button className="btn backproject-btn">Back this project</button>
+        <button className="btn bookmark-btn">Bookmark</button>
+      </div>
+    </section>
+  );
+}
