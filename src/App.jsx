4867d34b223998ca2b54b27d15a7858e7939b6c9
Manish <mjpatel84@gmail.com>
2023-12-05 07:15:58 -0500

    adds ProjectAbout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import ProjectMain from "./components/ProjectMain";
 import ProjectStats from "./components/ProjectStats";
+import ProjectAbout from "./components/ProjectAbout";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Header />
       <ProjectMain />
       <ProjectStats />
+      <ProjectAbout />
     </>
   );
 }
---
+++ src/components/ProjectAbout.jsx
@@ -0,0 +1,7 @@
+export default function ProjectAbout() {
+  return (
+    <section className="about-project">
+      <h2>About this project</h2>
+    </section>
+  );
+}
