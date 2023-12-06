e58483c12c76d9797079beeab2400612b84a9a2d
Manish <mjpatel84@gmail.com>
2023-12-06 06:20:57 -0500

    adds About and Rewards components and updates styles

--- src/components/ProjectAbout.jsx
+++ src/components/ProjectAbout.jsx
@@ -1,7 +1,22 @@
+import Rewards from "./Rewards";
+
 export default function ProjectAbout() {
   return (
     <section className="about-project">
       <h2>About this project</h2>
+      <p className="project-description">
+        The Mastercraft Bamboo Monitor Riser is a sturdy and stylish platform
+        that elevates your screen to a more comfortable viewing height. Placing
+        your monitor at eye level has the potential to improve your posture and
+        make you more comfortable while at work, helping you stay focused on the
+        task at hand.
+      </p>
+      <p className="project-description">
+        Featuring artisan craftsmanship, the simplicity of design creates extra
+        desk space below your computer to allow notepads, pens, and USB sticks
+        to be stored under the stand.
+      </p>
+      <Rewards />
     </section>
   );
 }
---
+++ src/components/Rewards.jsx
@@ -0,0 +1,54 @@
+export default function Rewards() {
+  return (
+    <div className="rewards">
+      <article className="reward">
+        <h3>Bamboo Stand</h3>
+        <p className="pledge">Pledge $25 or more</p>
+        <p className="pledge-description">
+          You get an ergonomic stand made of natural bamboo. You've helped us
+          launch our promotional campaign, and you’ll be added to a special
+          Backer member list.
+        </p>
+        <div className="reward-col">
+          <div className="reward-row">
+            <p className="statistic">101</p>
+            <h4>left</h4>
+          </div>
+          <button className="reward-btn">Select Reward</button>
+        </div>
+        {/* <button className="reward-btn">Select Reward</button> */}
+      </article>
+      <article className="reward">
+        <h3>Black Edition Stand</h3>
+        <p className="pledge">Pledge $75 or more</p>
+        <p className="pledge-description">
+          You get a Black Special Edition computer stand and a personal thank
+          you. You’ll be added to our Backer member list. Shipping is included.
+        </p>
+        <div className="reward-col">
+          <div className="reward-row">
+            <p className="statistic">64</p>
+            <h4>left</h4>
+          </div>
+          <button className="reward-btn">Select Reward</button>
+        </div>
+      </article>
+      <article className="reward reward-disabled">
+        <h3>Mahogany Special Edition</h3>
+        <p className="pledge">Pledge $200 or more</p>
+        <p className="pledge-description">
+          You get two Special Edition Mahogany stands, a Backer T-Shirt, and a
+          personal thank you. You’ll be added to our Backer member list.
+          Shipping is included.
+        </p>
+        <div className="reward-col">
+          <div className="reward-row">
+            <p className="statistic">0</p>
+            <h4>out of stock</h4>
+          </div>
+          <button className="reward-btn out-of-stock-btn">Out of stock</button>
+        </div>
+      </article>
+    </div>
+  );
+}
